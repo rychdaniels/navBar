62e952ff278723533fea116806bde93f7c58fa35
rych <rychraya@gmail.com>
2020-02-05 15:56:26 -0600

    Avance de recursividad

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 // Usamos las interfaces creadas
-import { RootObject, Menusroot } from '../interfaces/menu.interface';
+import { Menusroot } from '../interfaces/menu.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +10,16 @@ import { RootObject, Menusroot } from '../interfaces/menu.interface';
 export class InformacionService {
 
   // Se guarda la informacion de los menus
-  info: RootObject = {};
+  info: Menusroot = {};
   
   constructor( private http: HttpClient ) {
 
     // Leer el JSON gracias a el modulo http
     this.http.get('assets/data.json')
-      .subscribe( (resp: RootObject) => {
+      .subscribe( (resp: Menusroot) => {
         this.info = resp;
-      
-        console.log(this.info.menusroots[0].menus[1].menus[0].nombre);
+        
+        // console.log(this.info.menusroots[0].menus[1].menus[0].nombre);
         
         
       });
