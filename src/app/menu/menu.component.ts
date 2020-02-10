b73cc0881baaa006a997272883fd2ea6622a59e7
rych <rychraya@gmail.com>
2020-02-10 16:07:21 -0600

    Resuelto el problema para leer el JSON y CallStack Error

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { InformacionService } from '../services/informacion.service';
 import { Menusroot } from '../interfaces/menu.interface';
 
+import data from '../../assets/data.json';
 
 
 @Component({
@@ -15,92 +16,7 @@ import { Menusroot } from '../interfaces/menu.interface';
 
 export class MenuComponent implements OnInit {
   // Se guarda la informacion de los menus
-  public info: Menusroot[]= [
-    {
-       "id":1,
-       "nombre":"Menu Root 1",
-       "url":"",
-       "menus":[
-          {
-             "id":2,
-             "nombre":"Menu Hijo 1.1",
-             "url":"hijo1.html",
-             "menus":[
-                {
-                   "id":7,
-                   "nombre":"Menu Hijo 1.1.1",
-                   "url":"hijo211.html"
-                },
-                {
-                   "id":7,
-                   "nombre":"Menu Hijo 1.1.2",
-                   "url":"hijo211.html"
-                },
-                {
-                   "id":7,
-                   "nombre":"Menu Hijo 1.1.3",
-                   "url":"hijo211.html",
-                   "menus":[
-                     {
-                        "id":7,
-                        "nombre":"Menu Hijo 1.1.3.1",
-                        "url":"hijo211.html",
-                        "menus":[
-                           {
-                              "id":7,
-                              "nombre":"Menu Hijo 1.1.3.1.1",
-                              "url":"hijo211.html"
-                              
-                           }
-                        ]
-                     }
-                  ]
-                }
-             ]
-          },
-          {
-             "id":3,
-             "nombre":"Menu Hijo 2.1",
-             "url":"",
-             "menus":[
-                {
-                   "id":7,
-                   "nombre":"Menu Hijo 2.1.1",
-                   "url":"hijo211.html"
-                },
-                {
-                   "id":7,
-                   "nombre":"Menu Hijo 2.1.2",
-                   "url":"hijo211.html"
-                },
-                {
-                   "id":7,
-                   "nombre":"Menu Hijo 2.1.3",
-                   "url":"hijo211.html"
-                   
-                }
-             ]
-          }
-       ]
-    },
-    {
-       "id":4,
-       "nombre":"Menu Root 2",
-       "url":"",
-       "menus":[
-          {
-             "id":5,
-             "nombre":"Menu Hijo 2.1",
-             "url":"hijo1.html"
-          },
-          {
-             "id":6,
-             "nombre":"Menu Hijo 2.2",
-             "url":"hijo2.html"
-          }
-       ]
-    }
- ];
+  public info: Menusroot[]= data.menusroots;
    
   
   constructor( public infoMenuService: InformacionService ) { 
